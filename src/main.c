7cb9d6bcaaf5642a0fe539c697929793a542407e
Muhammad Anas Majeed Rafique <mumajeed@car7s6.42barcelona.com>
2025-01-13 13:06:18 +0100

    Libft_bonus

---
+++ src/main.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mumajeed <mumajeed@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/12 18:04:48 by mumajeed          #+#    #+#             */
+/*   Updated: 2025/01/12 18:40:35 by mumajeed         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/libft.h"
+
+int	main(int argc, char **argv)
+{
+	t_stack	*a;
+	t_stack	*b;
+
+	if (argc < 2)
+		return (0);
+	a = init_stack();
+	b = init_stack();
+	if (!parse_arguments(a, argc, argv))
+		error_exit("Error");
+	if (!is_sorted(a))
+		sort_stack(a, b);
+	clear_stack(a);
+	clear_stack(b);
+	return (0);
+}
---
+++ src/parser.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mumajeed <mumajeed@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/12 18:40:55 by mumajeed          #+#    #+#             */
+/*   Updated: 2025/01/12 18:52:20 by mumajeed         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/push_swap.h"
+
+static int	is_number(const char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	while (str[i])
+	{
+		if (!('0' <= str[i] && str[i] <= '9'))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	parse_arguments(t_stack *a, int argc, char **argv)
+{
+	int	i;
+	int	value;
+
+	i = 1;
+	while (i < argc)
+	{
+		if (!is_number(argv[i]))
+			return (0);
+		value = ft_atoi(argv[i]);
+		push(a, value);
+		i++;
+	}
+	return (1);
+}
